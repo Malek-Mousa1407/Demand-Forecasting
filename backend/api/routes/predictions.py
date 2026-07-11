5211306827943f7b03da7ce0957730e10ae4755a
Malek <malek.mousa1407@gmail.com>
2026-07-11 22:12:22 +0300

    Added Postgres connection files under /backend/database/. Imported database session into /backend/api/routes/predictions.py Deleted the old /predict/expect-demand and replaced it the /predict/expect-demand-many. Added logging feature.

--- backend/api/routes/predictions.py
+++ backend/api/routes/predictions.py
@@ -1,51 +1,43 @@
 import math
-
 import pickle
 import logging
 import warnings
-
 from pathlib import Path
+import sys
 from fastapi import APIRouter
 from schemas.DemandQuery import ProductInfo
 
 
+# Importing Database Connection and Schema
+backend_root = Path(__file__).resolve().parent.parent.parent
+if str(backend_root) not in sys.path:
+    sys.path.insert(0, str(backend_root))
+from database.database_config import SessionLocal
+import database.models as logging_table
+
+
+# Setting up logging 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
+# Importing ML model
 current_dir = Path(__file__).resolve().parent
 path_to_model = current_dir.parent.parent / "model" / "best_model.pkl"
-
 with open(path_to_model, 'rb') as file:
      model = pickle.load(file)
 model.set_params(verbosity=-1)
 
 
-
 router = APIRouter(prefix="/predict")
 
+
 @router.post('/expected-demand')
-def prediction(product: ProductInfo):
-    features = list(product.model_dump(exclude={"product_id"}).values())
-    prediction = model.predict([features], verbose = -1)[0]
-
-    forcatsed_demand = int(math.ceil(prediction))
-
-    re_order_amount = max(0, forcatsed_demand - product.current_stock_level)
-    
-    return {
-        "Product Info": product,
-        "Forecasted Weekly Demand": forcatsed_demand,
-        "Recommended Re-stock Amount": re_order_amount 
-    }
-
-
-
-@router.post('/expected-demand-many')
 def prediction(products: list[ProductInfo]):
     
     total_forcast = []
+    logs = []
 
     for product in products:
         features = list(product.model_dump(exclude={"product_id"}).values())
@@ -54,15 +46,31 @@ def prediction(products: list[ProductInfo]):
 
         prediction = model.predict([features])[0]
 
-        forcatsed_demand = int(math.ceil(prediction))
+        forcasted_demand = int(math.ceil(prediction))
 
-        re_order_amount = max(0, forcatsed_demand - product.current_stock_level)
+        re_order_amount = max(0, forcasted_demand - product.current_stock_level)
 
         total_forcast.append(
             {
                 "Product Info": product,
-                "Forecasted Weekly Demand": forcatsed_demand,
+                "Forecasted Weekly Demand": forcasted_demand,
                 "Recommended Re-stock Amount": re_order_amount 
             })
+        
+        logs.append(
+            logging_table.PredictionLog(
+                item_id = str(product.product_id),
+                prediction_value = forcasted_demand
+            )
+        )
+    # Writing the logs to the DB
+    try:
+        db = SessionLocal()
+        db.add_all(logs)
+        db.commit()
+
+    except Exception as e:
+        print("Error in writing logs to DB.")
+        db.rollback()
 
     return total_forcast
---
+++ backend/database/database_config.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/My_DB"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
